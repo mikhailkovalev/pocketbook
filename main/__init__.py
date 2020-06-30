67dda30e738c23f2cee520846c73f96026c7ef90
Mixon <mixon271@gmail.com>
2020-06-30 08:45:31 +0400

    Импорт pymysql обёрнут в try-except из-за ошибки на проде

--- main/__init__.py
+++ main/__init__.py
@@ -1,3 +1,7 @@
-import pymysql
-
-pymysql.install_as_MySQLdb()
+try:
+    # todo: узнать, нужно ли это на проде для
+    #  подключения к MySQL
+    import pymysql
+    pymysql.install_as_MySQLdb()
+except ImportError:
+    pass
