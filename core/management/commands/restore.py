30156b9ff08cf26e21c8397086549041cc071317
Mixon <mixon271@gmail.com>
2020-08-18 21:23:52 +0400

    core. Добавлена management-команда restore

---
+++ core/management/commands/restore.py
@@ -0,0 +1,51 @@
+from django.core.management import (
+    BaseCommand,
+    CommandParser,
+    call_command,
+)
+from django.core.management.commands.migrate import (
+    Command as MigrateCommand,
+)
+from django.core.management.commands.loaddata import (
+    Command as LoadDataCommand,
+)
+
+
+class Command(BaseCommand):
+    help: str = (
+        'Равносильно последовательному выполнению '
+        'команд `migrate` и `loaddata`.'
+    )
+
+    def __init__(self, *args, **kwargs) -> None:
+        super().__init__(*args, **kwargs)
+        self.stdout.ending = ''
+        self.stderr.ending = ''
+
+    def add_arguments(self, parser: CommandParser) -> None:
+        parser.add_argument(
+            'path_to_data',
+        )
+
+    def handle(self, path_to_data: str, **kw) -> None:
+        self._do_migrate()
+        self._do_load_data(path_to_data)
+
+    def _do_migrate(self) -> None:
+        migrate_command = MigrateCommand(
+            stdout=self.stdout,
+            stderr=self.stderr,
+        )
+        call_command(
+            migrate_command,
+        )
+
+    def _do_load_data(self, path_to_data: str) -> None:
+        load_data_command = LoadDataCommand(
+            stdout=self.stdout,
+            stderr=self.stderr,
+        )
+        call_command(
+            load_data_command,
+            path_to_data,
+        )
