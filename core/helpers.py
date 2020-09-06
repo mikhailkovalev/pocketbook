04e03ae91d2d56a34b44effa92f104b63d33133d
Mixon <mixon271@gmail.com>
2020-09-07 01:08:00 +0400

    core.  - Создана функция, оборачивающая продуцируемые объектом csv.reader tuple-ы в namedtuple-ы

--- core/helpers.py
+++ core/helpers.py
@@ -1,8 +1,29 @@
-from copy import copy
-from datetime import datetime
-from pytz import timezone
+import csv
 
-from django.conf import settings
+from collections import (
+    namedtuple,
+)
+from copy import (
+    copy,
+)
+from datetime import (
+    datetime,
+)
+from itertools import (
+    starmap,
+)
+from typing import (
+    Iterator,
+    NamedTuple,
+    TextIO,
+)
+
+from django.conf import (
+    settings,
+)
+from pytz import (
+    timezone,
+)
 
 
 def with_server_timezone(
@@ -18,3 +39,37 @@ def with_server_timezone(
     else:
         value = copy(value)
     return value
+
+
+def iterate_csv_by_namedtuples(
+        csvfile: TextIO,
+        delimiter: str = ';',
+        quotechar: str = '"',
+        typename: str = 'Row',
+) -> Iterator[NamedTuple]:
+    reader = csv.reader(
+        csvfile=csvfile,
+        delimiter=delimiter,
+        quotechar=quotechar,
+    )
+    row_type = namedtuple(
+        typename=typename,
+        field_names=next(reader),
+    )
+
+    # Положим row -- строка данными прочитанными из csv:
+    #   row = next(reader)
+    #
+    # Используя метод _make
+    #   named_row = row_type._make(row)
+    #
+    # избегаем создания промежуточного tuple используя
+    #   named_row = row_type(*row)
+    #
+    # todo: проверить, будет ли это более эффективным
+    #  чем использование itertools.starmap:
+    #    return starmap(row_type, reader)
+    return map(
+        row_type._make,  # noqa
+        reader,
+    )
