0e516c8b1be3bc30ddb4f2c4a3e4e6ce090cbaa1
Mixon <mixon271@gmail.com>
2020-09-20 17:46:55 +0400

    money.  - описаны аргументы команды  - получаем пользователя и строки файла

--- money/management/commands/load_providers.py
+++ money/management/commands/load_providers.py
@@ -1,13 +1,49 @@
+from typing import (
+    Iterator,
+    NamedTuple,
+)
+
+from django.contrib.auth.models import (
+    User,
+)
 from django.core.management import (
     BaseCommand,
     CommandParser,
 )
 
+from core.helpers import (
+    iterate_csv_by_namedtuples,
+)
+
+
+class RowType(NamedTuple):
+    name: str
+    parent: str
+
 
 class Command(BaseCommand):
 
     def add_arguments(self, parser: CommandParser):
-        pass
+        super().add_arguments(parser)
+        parser.add_argument(
+            'username',
+        )
+        parser.add_argument(
+            'file_path',
+        )
 
-    def handle(self, *args, **options):
-        pass
+    def handle(self, username, file_path, *args, **options):
+        user_id = User.objects.filter(
+            username=username,
+        ).values_list(
+            'id',
+            flat=True,
+        ).get()
+
+        source = open(file_path, 'rt', encoding='utf-8')
+        items: Iterator[RowType] = iterate_csv_by_namedtuples(
+            source,
+            rowtype=RowType,
+            delimiter=';',
+        )
+
