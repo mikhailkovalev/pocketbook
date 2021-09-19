bede60dbe5d2ccf97418281e660e2caa298eba31
Mixon <mixon271@gmail.com>
2021-09-19 15:24:58 +0300

    промежуточный коммит

--- tests/sugar/test_insulin_syringe_admin.py
+++ tests/sugar/test_insulin_syringe_admin.py
@@ -1,3 +1,5 @@
+from unittest.mock import ANY
+
 import pytest
 from lxml import (
     etree,
@@ -28,15 +30,56 @@ def test_add_record(
     client = create_client(
         authenticated_with=admin,
     )
-    response = client.get(url)
 
+    response = client.get(url)
     assert response.status_code == 200
 
-    with open('/home/mixon/.config/JetBrains/PyCharmCE2021.2/scratches/syringes_add.html', 'wb') as f:
-        f.write(response.content)
+    etree_html_parser = etree.HTMLParser()
+    tree = etree.XML(
+        text=response.content.decode('utf=8'),
+        parser=etree_html_parser,
+    )
 
-    # etree_html_parser = etree.HTMLParser()
-    # tree = etree.XML(
-    #     text=response.content.decode('utf=8'),
-    #     parser=etree_html_parser,
-    # )
+    insulin_kind_value = tree.xpath(
+        '//div[@class="form-row field-insulin_mark"]'
+        '/div'
+        '/div'
+        '/select[@id="id_insulin_mark"]'
+        '/option[@selected]'
+        '/text()'
+    )
+    assert insulin_kind_value == [
+        '---------',
+    ]
+
+    volume_value = tree.xpath(
+        '//div[@class="form-row field-volume"]'
+        '/div'
+        '/input'
+        '/@value'
+    )
+    assert volume_value == []
+
+    opening_value = tree.xpath(
+        '//div[@class="form-row field-opening"]'
+        '/div'
+        '/input'
+        '/@value'
+    )
+    assert opening_value == []
+
+    expiry_plan_value = tree.xpath(
+        '//div[@class="form-row field-expiry_plan"]'
+        '/div'
+        '/input'
+        '/@value'
+    )
+    assert expiry_plan_value == []
+
+    expiry_actual_value = tree.xpath(
+        '//div[@class="form-row field-expiry_actual"]'
+        '/div'
+        '/input'
+        '/@value'
+    )
+    assert expiry_actual_value == []
